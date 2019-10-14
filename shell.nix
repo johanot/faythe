64a41eace061c636c00b45c4cbdc9a005b79e179
Johan Thomsen <jth@dbc.dk>
2019-10-14 08:03:56 +0200

    update shell pin

--- shell.nix
+++ shell.nix
@@ -4,8 +4,8 @@ let
   pkgs = import (_pkgs.fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs-channels";
-    rev = "e19054ab3cd5b7cc9a01d0efc71c8fe310541065";
-    sha256 = "0b92yhkj3pq58svyrx7jp0njhaykwr29079izqn6qs638v8zvhl2";
+    rev = "222004e52e82ae7b827b20184d25c1ce88b85da6";
+    sha256 = "00wv3bpyqp8cc2rb8mnfdy5xv28bn43qal9316jfmczdmnpvqpyg";
   }) {
     overlays = [moz_overlay];
   };
