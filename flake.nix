035d9c028f9a93c914cb18efc073e601b506745c
Johan Thomsen <write@ownrisk.dk>
2023-02-25 22:59:29 +0100

    crate2nix -> crane
    (cherry picked from commit 1804962995d460f3ea72c030388a6059f5c7bf01)

--- flake.nix
+++ flake.nix
@@ -2,27 +2,52 @@
   description = "faythe";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-    ci.url = "git+https://gitlab.dbc.dk/platform/bump-o-matic.git";
-    ci.inputs.nixpkgs.follows = "nixpkgs";
-    ci.inputs.utils.follows = "utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
+    crane.inputs.flake-utils.follows = "utils";
   };
 
-  outputs = { self, nixpkgs, ci, utils }:
+  outputs = { self, crane, nixpkgs, utils }:
   let
     pname = "faythe";
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
-      overlays = [ ci.overlay ];
+      overlays = [ crane-overlay self.overlays.default ];
+    };
+    crane-overlay = final: prev: {
+      # crane's lib is not exposed as an overlay in its flake (should be added
+      # upstream ideally) so this interface might be brittle, but avoids
+      # accidentally passing a detached nixpkgs from its flake (or its follows)
+      # on to consumers.
+      craneLib = crane.mkLib final;
     };
   in {
-    packages.${system}.${pname} = (import ./Cargo.nix {
-      inherit pkgs;
-    }).rootCrate.build;
+    packages.${system}.${pname} = pkgs.${pname};
+    defaultPackage.${system} = pkgs.${pname};
 
-    defaultPackage.${system} = self.packages.${system}.${pname};
+    overlays.default = final: prev: {
+      "${pname}" = final.craneLib.buildPackage {
+        src =
+          let
+            srcPath = ./.;
+          in
+            with final; lib.cleanSourceWith {
+              src = srcPath;
+              filter = path: type:
+                craneLib.filterCargoSources path type ||
+                lib.hasPrefix "${toString srcPath}/test" path;
+            };
+        nativeBuildInputs = with final; [
+          pkg-config
+        ];
+        buildInputs = with final; [
+          openssl
+        ];
+      };
+    };
 
     devShell.${system} = with pkgs; mkShell {
       buildInputs = [
@@ -34,8 +59,6 @@
         zlib.dev
         dnsutils # runtime
         kubectl # runtime
-        bump-o-matic
-        bump-rust
       ];
     };
   };
